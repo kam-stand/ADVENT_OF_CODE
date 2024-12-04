a4693e540575702db4fa31e69fb9bc8569971a0b
kam-stand <kamrul.hassan@stonybrook.edu>
2024-12-03 22:17:19 -0500

    Added readme

---
+++ DAY_3/main.c
@@ -0,0 +1,15 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+//  put all the contents of the string into an array
+//  each index of the array is mentioned by a '\n'
+int main(void){
+
+    
+
+
+    return 0;
+}
